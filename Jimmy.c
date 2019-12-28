3e260291d94325904eab51709cba13eef23d2c79
Arda Samadi <arda.samadi2010@gmail.com>
2019-12-28 12:55:20 +0330

    ridam too code

--- Jimmy.c
+++ Jimmy.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-
-
+#include <stdlib.h>
+#include <time.h>
 int main()
 {
+	srand(time(0));
 	while (1)
 	{
-		printf("%d",rand(38)%2);
+		printf("%d",(rand()%2));
 	}
 return 0;
 }
