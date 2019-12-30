371bcdb4b468ebf8f2fa31876a0bb2ba6d7286da
ArdaSamadi <arda.samadi2010@gmail.com>
2019-12-30 17:51:19 +0330

    hack fbi

--- Jimmy.c
+++ Jimmy.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 int main()
 {
+	char array[100];
+	char lol[]={"Hack fbi"};
+	int i;	
+	scanf("%[^\n]",array);
 	srand(time(0));
+	if (!strcmp(array,lol))
 	while (1)
 	{
 		printf("%d",(rand()%2));
+//		if (rand()%10000==0)
+//			for (i=0;i<10;i++)
+//			printf("COOOOOOOOCKKKKKKKKK!!\n\n\n\n\n\n\n");
 	}
-return 0;
+	return 0;
 }
